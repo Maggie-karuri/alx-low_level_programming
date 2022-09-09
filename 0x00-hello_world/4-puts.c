bd756db7a21720ab4ee95a8e616e1136bd31418a
Maggie-karuri <karurimargaret111@gmail.com>
2022-09-08 17:16:50 -0700

    puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * Description: main - Prints out a string to stdout.
+ * Return: 0 if success.
+ */
+int main(void)
+{
+	/*string initialiation*/
+	puts("\"Programming is like building a multilingual puzzle");
+	/*writing the string to stdout*/
+
+	return (0);
+}

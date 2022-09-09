e3e8d8af8542aa1a4f809130ab68038b540531c5
Maggie-karuri <karurimargaret111@gmail.com>
2022-09-08 18:20:57 -0700

    scan

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**declaring funtions
+ * main =entry
+ * Return 1 if successful*/
+int main(void)
+{
+/*initialiation*/
+	scanf("and that piece of art is useful\" - Dora Korpar, 2015-10-19",);
+/*scan the content and give it as output*/
+
+return 0;
+}
+

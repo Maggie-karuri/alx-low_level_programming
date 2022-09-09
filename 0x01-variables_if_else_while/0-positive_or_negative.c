c142dc6a57e74908998eee941651b16c70196b24
Maggie-karuri <karurimargaret111@gmail.com>
2022-09-09 16:47:24 -0700

    positive or negative

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -6,7 +6,7 @@
  * main- prints whether the value is positive, zero or negative.
  *
  * Description: uses the main funtion
- * The program checks if the value is positive, negative, or zero
+ * The program prints "if the value is positive, negative, or zero"
  * Return 0 if successfull
  */
 int main(void)

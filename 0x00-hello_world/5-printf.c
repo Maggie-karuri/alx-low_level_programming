f66802f810007c6e4215d83586534823e8689a7d
Maggie-karuri <karurimargaret111@gmail.com>
2022-09-08 17:35:35 -0700

    printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * Description: main - Prints out a string to stdout.
+ * Return: 0 if success.
+ */
+int main(void)
+{
+	/* initialiation */
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	/*get output using printf*/
+
+return (0);
+}
+

7480b11db45ad4684c36ff211b0396c6509c9a1d
Maggie-karuri <karurimargaret111@gmail.com>
2022-09-09 17:56:48 -0700

    1

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -8,7 +8,7 @@
  * it executes, and printsthe value of n
  * Return 0 if successfull
  */
-/* main - assigns a random number to int n everytime*/
+/* main - assigns a random number and return 0 if succesful*/
 int main(void)
 {
 int n;

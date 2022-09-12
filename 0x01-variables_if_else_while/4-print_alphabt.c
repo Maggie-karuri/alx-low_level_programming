0eab8893b9840dec25f5fc39ff55b2088d0f4567
Maggie-karuri <karurimargaret111@gmail.com>
2022-09-12 15:04:47 -0700

    except

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Description: the program prints all alphabets in lower cases except q and e
+ * followed by a new line
+ * Return : always 0 success
+ */
+int main(void)
+{
+char ch;
+
+for (ch = 'a'; ch <= 'z'; ch++)
+	if ch == 'e' or ch == 'q'
+continue
+{
+putchar ch;
+}
+putchar('\n');
+return (0);
+}

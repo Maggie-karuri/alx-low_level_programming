cf49d5d5d8230818a325d9ea273e9275fc97f1ce
Maggie-karuri <karurimargaret111@gmail.com>
2022-09-26 13:22:56 -0700

    mfill memory

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * _memset - fill memory
+ * @s - input string
+ * @b - input character
+ * @n - input size
+ *
+ * Description: fill memory with cost char
+ *
+ * return: returns the pointer to s
+ */
+
+char *_memset(char *s, char b, unsigned int n)
+{
+unsigned int i;
+
+for (i = 0; i < n; i++)
+{
+	s[i] = b;
+}
+return (s);
+}

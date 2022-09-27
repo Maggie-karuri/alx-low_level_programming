584d21eb3a32a5725c82ca7394320981f97505cc
Maggie-karuri <109177261+Maggie-karuri@users.noreply.github.com>
2022-09-27 09:50:25 +0300

    string-character

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -2,12 +2,28 @@
 #include <stdio.h>
 
 /**
+ * _strchr - locats a character
+ * @s: input string
+ * @c: input character
  *
- *
- *
+ * Description: locates a character in a string
+ * Return: returns a ptr to the 1st occurrence of
+ * the char c in str s, else, return NULL
  */
 
 char *_strchr(char *s, char c)
 {
-
+	while (*s)
+	{
+		if (c == *s)
+		{
+			return (s);
+		}
+		s++;
+	}
+	if (!c)
+	{
+		return (s);
+	}
+	return (NULL);
 }
